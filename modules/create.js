74bb83566d302d7a742382ea0199a6c30364ee90
Vladimir <kartayev@gmail.com>
2020-08-29 22:20:48 +0300

    problems with router

--- modules/create.js
+++ modules/create.js
@@ -1,4 +1,4 @@
-define(["modules/fetchData"], (fetchData) => {
+define(["modules/fetchData", "dojo/dom-construct"], (fetchData, construct) => {
   return {
     createMeals: (value, node1, node2) => {
       const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`;
@@ -12,7 +12,7 @@ define(["modules/fetchData"], (fetchData) => {
                       <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
 
                       <div class="meal-info" data-mealID="${meal.idMeal}">
-                          <h3>${meal.strMeal}</h3>
+                          <h3 class="meal-title">${meal.strMeal}</h3>
                       </div>
                   </div>
             `
@@ -20,12 +20,19 @@ define(["modules/fetchData"], (fetchData) => {
           .join("");
       });
     },
-    createSingleMeal: (id, node1) => {
+    createSingleMeal: (id, node1, node2) => {
       const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
 
       fetchData.getMeals(url).then(({ meals }) => {
         const meal = meals[0];
         node1.innerHTML = `<h2>${meal.strMeal} recipe:</h2>`;
+        // construct.place(
+        //   `<div class="meal">
+        //     <img src="${meal.strMealThumb}"/>
+        //   </div>`,
+        //   node2,
+        //   "replace"
+        // );
       });
     },
   };
